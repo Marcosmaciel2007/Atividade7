ecb1ce76fd1c26a3eaf3719367bd37fb05be618c
Marcosmaciel2007 <79186614+Marcosmaciel2007@users.noreply.github.com>
2021-05-03 03:37:43 -0300

    2

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SomandoValor</title>
+    <title>Somando Valor</title>
 </head>
 <body>
     <form action="soma.php" method="post">
-        <label for="dados">Insira uma lista de valores númericos:</label><br>
+        <label for="dados">Insira uma lista de valores numéricos:</label><br>
         <textarea name="dados" id="dados" rows="5"></textarea><br>
         <input type="submit" value="Enviar">
     </form>
